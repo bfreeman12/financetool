bbe98ec6347839fa0a315a75d7b7d52cba652cd8
bfreeman12 <bfree98@icloud.com>
2023-11-22 17:08:45 +0100

    adding foundation for form filling

--- client/src/pages/Forms.jsx
+++ client/src/pages/Forms.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useLocation } from "react-router-dom";
 
 const Forms = () => {
   let { state } = useLocation();
+  const data = [...state?.data];
+  const [filledData, setFilledData] = useState({});
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    data.shift();
+    if (data.length === 0) {
+      alert("no more forms");
+      data = [];
+    }
+  };
   return (
     <>
       <Navbar />
-      {state?.data?.length === 0 || !state?.data ? (
-        <p style={{ color: "white" }}>No data found</p>
-      ) : (
-        <h1 style={{ color: "white" }}>{state?.data}</h1>
-      )}
+      <div className="form-body">
+        <button onClick={(e) => handleClick(e)}>next form</button>
+      </div>
       <Footer />
     </>
   );
