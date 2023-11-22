4feae68b1928f815dca0ff43dc51e439e1117e8e
bfreeman12 <bfree98@icloud.com>
2023-11-22 17:42:02 +0100

    added form queue

--- client/src/pages/Forms.jsx
+++ client/src/pages/Forms.jsx
@@ -2,29 +2,50 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useLocation } from "react-router-dom";
-
+import "../styles/forms.css";
 const Forms = () => {
   let { state } = useLocation();
-  const data = [...state?.data];
+  const [data, setData] = useState([...state?.data]);
   const [filledData, setFilledData] = useState({});
 
   const handleClick = (e) => {
     e.preventDefault();
-    data.shift();
     if (data.length === 0) {
       alert("no more forms");
-      data = [];
+    } else {
+      setData(data.slice(1));
     }
   };
+
+  const DisplayData = () => {
+    if (data.length === 0) {
+      return null;
+    }
+
+    return (
+      <ul className="data-list">
+        {data.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <>
       <Navbar />
+      <div className="form-list">
+        <DisplayData />
+      </div>
       <div className="form-body">
-        <button onClick={(e) => handleClick(e)}>next form</button>
+        {data.length ? (
+          <button onClick={(e) => handleClick(e)}>next form</button>
+        ) : (
+          <label>no more form</label>
+        )}
       </div>
       <Footer />
     </>
   );
 };
-
 export default Forms;
