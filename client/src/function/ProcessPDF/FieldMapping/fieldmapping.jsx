2f15cad83d45139e0a1012213b9f09428a0a1f6e
bfreeman12 <bfree98@icloud.com>
2024-01-29 16:01:28 +0100

    leaving it for today

--- client/src/function/ProcessPDF/FieldMapping/fieldmapping.jsx
+++ client/src/function/ProcessPDF/FieldMapping/fieldmapping.jsx
@@ -4,6 +4,7 @@ When adding more forms ensure the key matches the shortFormUrl variable in the p
 */
 
 const fieldMapping = (pdf) => {
+  console.log(pdf);
   const fields = {
     "2231 Direct Deposit Form": {
       SSN: pdf.userProfile.ssn,
@@ -36,6 +37,45 @@ const fieldMapping = (pdf) => {
       "Allotment Acct Title": pdf.payrollFields.allotteeTitle,
       "Allottment FI Name": pdf.payrollFields.allotteeBankName,
       "FI Name": pdf.payrollFields.bankName,
+      Date: pdf.userProfile.todaysDate,
+    },
+    "daf594.pdf": {},
+    "DD 1351-2c.pdf": {},
+    "DD Form 1561.pdf": {
+      Name_Of_Member:
+        pdf.userProfile.lastName +
+        ", " +
+        pdf.userProfile.firstName +
+        " " +
+        pdf.userProfile.middleInitial,
+      Grade: pdf.userProfile.userGrade,
+      DoDID: pdf.userProfile.dodId,
+      Branch_And_Organization:
+        pdf.userProfile.userBranch + " " + pdf.fsaFields.org,
+      Date_Departed_Residence: pdf.fsaFields.dateDeparted,
+      Dependents_Addresses: pdf.fsaFields.dependentAddress,
+      // "form1[0].Table4[0].Row2[0].RadioButtonList[0]":
+      // pdf.fsaFields.fsaType || "",
+      //certification checkboxes
+      a_Check: "Check",
+      b_Check: pdf.fsaFields.legalCustody,
+      c_Check: pdf.fsaFields.dependentMilitary,
+      d_Check: pdf.fsaFields.institution,
+      e_Check: pdf.fsaFields.parents,
+      f_Check: pdf.fsaFields.marriedToActive,
+      g_Check: pdf.fsaFields.wasResiding,
+      Spouse_DoDID: pdf.fsaFields.spouseDodId,
+      Branch_And_Component: pdf.fsaFields.spouseBranchComponent,
+
+      Member_Signature_Date: pdf.userProfile.todaysDate,
+    },
+    "DD FORM 2560 pay advance.pdf": {},
+    "Lost Receipt Form.pdf": {},
+    "State of Legal Residence - DD Form 2058.pdf": {
+      Name: pdf.userProfile.lastName + ", " + pdf.userProfile.firstName,
+      DoDID: pdf.userProfile.dodId,
+      Legal_Residence: pdf.legalResidenceFields.legalResidence,
+      Mailing_Address: pdf.legalResidenceFields.mailingAddress,
       Date: pdf.userProfile.todaysDate,
     },
   };
